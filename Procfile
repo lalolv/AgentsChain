59301fca2dd09815269b3dfe7a83aa440cc464d9
Lalo <work@lalo.im>
2023-07-24 14:45:10 +0800

    Add docker config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -b :8080 -w 4 main:app -k uvicorn.workers.UvicornWorker
--- main.py
+++ main.py
@@ -9,6 +9,11 @@ load_dotenv()
 # create app
 app = FastAPI()
 
+# 默认路径
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
 # 路由设置
 app.include_router(system.router)
 app.include_router(user.router)

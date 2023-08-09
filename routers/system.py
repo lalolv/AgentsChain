06158d4cd16abf7ec1e5da91728f3295d9cbaca4
Lalo <work@lalo.im>
2023-08-09 10:44:20 +0800

    Update ver 0.0.3

--- routers/system.py
+++ routers/system.py
@@ -10,7 +10,7 @@ async def get_sys_info():
     openai_type = os.environ.get('OPENAI_API_TYPE')
     return {
         "status": "ok",
-        "version": "0.0.2",
+        "version": "0.0.3",
         "author": "Lalo",
         "openai_type": openai_type
     }
